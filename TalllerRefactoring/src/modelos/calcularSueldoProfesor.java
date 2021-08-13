51f3b12e5e30a7168cf94913508822be7061d16a
Victor Peña Barragán <70599635+vicpebarragan@users.noreply.github.com>
2021-08-12 22:19:06 -0500

    Refactoring Temporary Field

--- TalllerRefactoring/src/modelos/calcularSueldoProfesor.java
+++ TalllerRefactoring/src/modelos/calcularSueldoProfesor.java
@@ -3,8 +3,6 @@ package modelos;
 public class calcularSueldoProfesor {
     
     public double calcularSueldo(Profesor prof){
-        double sueldo=0;
-        sueldo= prof.info.añosdeTrabajo*600 + prof.info.BonoFijo;
-        return sueldo;
+        return prof.info.añosdeTrabajo*600 + prof.info.BonoFijo;
     }  
 }
